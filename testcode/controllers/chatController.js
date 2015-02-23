7dbdefefef01fd5f4e965f5c15c79981a4b1378c
Kirsten <kdawes@uoregon.edu>
2015-02-23 07:48:12 -0800

    start html is login

---
+++ testcode/controllers/chatController.js
@@ -0,0 +1,24 @@
+var chatApp = angular.module('mainApp.chatApp', ['ngCookies']);
+chatApp.controller('chatController', function($scope, $interval) {
+    $scope.names=['Jani','Hege','Kai'];
+    $interval(function(){
+        //alert($scope.names);
+        $scope.names[$scope.names.length] = $scope.names[$scope.names.length-1] + 'z';
+    },2000);
+
+
+     $scope.addFriend = function(name){
+        alert("add "+name);
+    };
+    $scope.chat = function(name){
+        alert("chat "+name);
+    };
+    $scope.challenge = function(name){
+        alert("challenge "+name);
+    };
+    $scope.send = function(){
+        alert($scope.msg);
+    };
+
+});
+
---
+++ testcode/controllers/loginController.js
@@ -0,0 +1,32 @@
+
+   'use strict'
+    var loginApp = angular.module('mainApp.loginApp', ['ngCookies',]);
+    loginApp.controller('loginController', function($scope, $location, $window, $cookieStore, $http) {
+
+        //for handling login submit
+        $scope.submitLogin = function(){
+            //TODO: send request to server, set corresponding cookies
+            if ($scope.userL.uname == "guest" && $scope.userL.pass == "guest"){
+                alert("login successful!");
+
+                //send request to django server and store cookie if successful
+                $http.get('http://www.w3schools.com/website/Customers_JSON.php').
+                  success(function(data, status, headers, config) {
+                      alert(data);
+                      $cookieStore.put('test', 'testcookie');
+                      $window.location.href = document.URL.substr(0,document.URL.lastIndexOf('/')+1)+"../index.html";
+                  }).
+                  error(function(data, status, headers, config) {
+                      alert(status);
+                  });
+            }else{
+                alert("login failed!");
+            }
+        }
+
+        //for handling register submit
+        $scope.submitReg = function(){
+            alert($scope.usernameReg);
+            //TODO: send request to server
+        }
+    });
