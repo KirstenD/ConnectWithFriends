77aebc56cbe4eda34bff892e42228bfc61d0a303
morgan <uoozhang@gmail.com>
2015-02-23 00:01:50 -0800

    more interfaces

--- testcode2/connect4App.js
+++ testcode2/connect4App.js
@@ -5,4 +5,17 @@ connect4App.controller('connect4Controller', function($scope, $interval) {
         //alert($scope.names);
         $scope.names[$scope.names.length] = $scope.names[$scope.names.length-1] + 'z';
     },2000);
+    $scope.addFriend = function(name){
+        alert("add "+name);
+    };
+    $scope.chat = function(name){
+        alert("chat "+name);
+    };
+    $scope.challenge = function(name){
+        alert("challenge "+name);
+    };
+    $scope.send = function(){
+        alert($scope.msg);
+    };
+
 });
