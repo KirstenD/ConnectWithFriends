a0373abfdb3ef50b7b28adbfad0a804dde5ca3af
Skyler Berg <skylertheberg@gmail.com>
2015-02-25 23:20:16 -0800

    Change chat send url to "send"

--- src/backEnd/chat/urls.py
+++ src/backEnd/chat/urls.py
@@ -4,5 +4,5 @@ from chat import views
 
 urlpatterns = patterns('',
     url(r'^index$', views.global_message_list, name='index'),
-    url(r'^global$', views.send_global_message, name='detail'),
+    url(r'^send$', views.send_global_message, name='detail'),
 )
