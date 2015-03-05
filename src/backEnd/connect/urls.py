68dbee030bc8bd0bef04df5799743a50924028c0
Skyler Berg <skylertheberg@gmail.com>
2015-03-05 10:40:55 -0800

    Add friends application

--- src/backEnd/connect/urls.py
+++ src/backEnd/connect/urls.py
@@ -4,5 +4,6 @@ from django.contrib import admin
 urlpatterns = patterns('',
     url(r'^accounts/', include('accounts.urls', namespace="accounts")),
     url(r'^chat/', include('chat.urls', namespace="chat")),
+    url(r'^friends/', include('friends.urls', namespace="friends")),
     url(r'^admin/', include(admin.site.urls)),
 )
