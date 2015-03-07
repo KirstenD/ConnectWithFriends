47b3fee967de28de87383ab44c926f754983f2d0
Skyler Berg <skylertheberg@gmail.com>
2015-03-07 14:29:00 -0800

    Add games app

--- src/backEnd/connect/urls.py
+++ src/backEnd/connect/urls.py
@@ -5,5 +5,6 @@ urlpatterns = patterns('',
     url(r'^accounts/', include('accounts.urls', namespace="accounts")),
     url(r'^chat/', include('chat.urls', namespace="chat")),
     url(r'^friends/', include('friends.urls', namespace="friends")),
+    url(r'^games/', include('games.urls', namespace="games")),
     url(r'^admin/', include(admin.site.urls)),
 )
