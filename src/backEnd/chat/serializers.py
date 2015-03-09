ba8644d7172d9b2e38f8e0bdfb0be4223fd43e7c
Skyler Berg <skylertheberg@gmail.com>
2015-03-08 21:21:51 -0700

    Add game chat

--- src/backEnd/chat/serializers.py
+++ src/backEnd/chat/serializers.py
@@ -1,12 +1,15 @@
-from django.contrib.auth.models import User
 from rest_framework import serializers
 
-from chat.models import GlobalMessage
-from accounts.serializers import UserSerializer
+from chat.models import GlobalMessage, GameMessage
 
 
 class GlobalMessageSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = GlobalMessage
         fields = ('sender', 'text', 'pub_date')
+
+
+class GameMessageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = GameMessage
+        fields = ('sender', 'text', 'pub_date', 'game')
--- src/backEnd/chat/views.py
+++ src/backEnd/chat/views.py
@@ -4,8 +4,9 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
 
-from chat.models import GlobalMessage
-from chat.serializers import GlobalMessageSerializer
+from chat.models import GlobalMessage, GameMessage
+from chat.serializers import GlobalMessageSerializer, GameMessageSerializer
+from games.models import Game
 
 
 @api_view(['GET'])
@@ -25,6 +26,38 @@ def send_global_message(request):
     data["sender"] = request.user.pk
     data["pub_date"] = timezone.now()
     serializer = GlobalMessageSerializer(data=data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=201)
+    return Response(serializer.errors, status=400)
+
+
+@api_view(['GET'])
+@permission_classes((IsAuthenticated, ))
+def game_message_list(request):
+    """
+    List all messages for the current game this user is in.
+    """
+    game = Game.current_game(request.user)
+    if not game:
+        return Response({"detail": "You are not in a game."}, 400)
+    game_messages = GameMessage.objects.filter(game=game)
+    game_messages = GameMessage.objects.all()
+    serializer = GameMessageSerializer(game_messages, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+@permission_classes((IsAuthenticated, ))
+def send_game_message(request):
+    game = Game.current_game(request.user)
+    if not game:
+        return Response({"detail": "You are not in a game."}, 400)
+    data = JSONParser().parse(request)
+    data["sender"] = request.user.pk
+    data["pub_date"] = timezone.now()
+    data["game"] = game.pk
+    serializer = GameMessageSerializer(data=data)
     if serializer.is_valid():
         serializer.save()
         return Response(serializer.data, status=201)
