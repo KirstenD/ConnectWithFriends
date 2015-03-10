f423fe599a54850f9ad37ac64e1a1f014f019cbd
Skyler Berg <skylertheberg@gmail.com>
2015-03-10 15:46:46 -0700

    Add extra fields to chat message returns

--- src/backEnd/chat/serializers.py
+++ src/backEnd/chat/serializers.py
@@ -4,12 +4,30 @@ from chat.models import GlobalMessage, GameMessage
 
 
 class GlobalMessageSerializer(serializers.ModelSerializer):
+    sender_name = serializers.SerializerMethodField()
+    display_time = serializers.SerializerMethodField()
+
     class Meta:
         model = GlobalMessage
-        fields = ('sender', 'text', 'pub_date')
+        fields = ('sender', 'text', 'pub_date', 'sender_name', 'display_time')
+
+    def get_sender_name(self, obj):
+        return obj.sender.username
+    
+    def get_display_time(self, obj):
+        return obj.pub_date.strftime("%B %d, %I:%M:%S %p")
 
 
 class GameMessageSerializer(serializers.ModelSerializer):
+    sender_name = serializers.SerializerMethodField()
+    display_time = serializers.SerializerMethodField()
+
     class Meta:
         model = GameMessage
-        fields = ('sender', 'text', 'pub_date', 'game')
+        fields = ('sender', 'text', 'pub_date', 'game', 'sender_name', 'display_time')
+
+    def get_sender_name(self, obj):
+        return obj.sender.username
+    
+    def get_display_time(self, obj):
+        return obj.pub_date.strftime("%B %d, %I:%M:%S %p")
